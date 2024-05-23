efff1ceb53cc83a48d2a17f89c1a5a463172c6b1
moojica <marcmujica@yahoo.com>
2024-05-22 20:41:09 -0400

    Updated JS logic to handle showing and hiding the newsletter form and success page

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const form = document.querySelector('.newsletter-form');
 const invalidEmailSpan = document.getElementById('invalid-email-text');
 const input = document.getElementById('email');
+const dissmissButton = document.getElementById('dissmiss-btn');
+const leftCard = document.getElementById('left-card');
+const rightCard = document.getElementById('right-card');
+const successPage = document.getElementById('success-page');
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
@@ -13,7 +17,24 @@ form.addEventListener('submit', function (e) {
     invalidEmailSpan.classList.remove('invalid');
     invalidEmailSpan.classList.add('hidden');
     input.classList.remove('invalid');
+
+    successPage.classList.add('success-page');
+    leftCard.classList.add('hidden');
+    rightCard.classList.add('hidden');
   }
+});
+
+dissmissButton.addEventListener('click', function () {
+  // Show newsletter form
+  leftCard.classList.remove('hidden');
+  rightCard.classList.remove('hidden');
+
+  // reset email input field to be empty
+  input.value = '';
+
+  // hide success page
+  successPage.classList.remove('success-page');
+  successPage.classList.add('hidden');
 });
 
 function validEmail() {
